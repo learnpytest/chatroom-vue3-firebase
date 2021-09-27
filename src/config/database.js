f98fd9c2f98cfd68d7b7c56a7f8686d0eb44f713
RachelChen <iceage91@gmail.com>
2021-09-27 21:05:45 +0800

    Install firebase and add database.js to config firebase service

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+import {
+  initializeApp
+} from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_APIKEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID
+}
+
+const app = initializeApp(firebaseConfig)
+
+export default app
