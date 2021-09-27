e1e02825073a039cf35e34a6454b667e9eb38280
RachelChen <iceage91@gmail.com>
2021-09-27 20:57:52 +0800

    Install dotenv and add firebase values to .env file to save secret key

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { createApp } from 'vue'
+import {
+  createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dotenv from 'dotenv'
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 createApp(App).use(store).use(router).mount('#app')
